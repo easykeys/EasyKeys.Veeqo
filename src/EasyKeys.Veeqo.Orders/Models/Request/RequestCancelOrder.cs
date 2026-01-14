f85d89643f12688aaae528b995bc6c8df9303d89
Brandon Moffett <bmoffett@easykeys.com>
2026-01-13 18:04:52 -0800

    Set default CancelReason to "unspecified" in RequestCancelOrder
    Changed the default value of CancelReason from an empty string to "unspecified" to ensure a meaningful reason is provided when none is specified.

--- src/EasyKeys.Veeqo.Orders/Models/Request/RequestCancelOrder.cs
+++ src/EasyKeys.Veeqo.Orders/Models/Request/RequestCancelOrder.cs
@@ -8,7 +8,7 @@ public class RequestCancelOrder
     public int OrderId { get; set; }
 
     [JsonPropertyName("cancel_reason")]
-    public string CancelReason { get; set; } = "";
+    public string CancelReason { get; set; } = "unspecified";
 
     [JsonPropertyName("send_veeqo_email")]
     public bool SendVeeqoEmail { get; set; } = false;
